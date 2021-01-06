54e76fa2ba127e7dea5b1ad70bb2d8d8190c6264
wangzhibo <coder_lufei@163.com>
2021-01-06 16:41:35 +0800

    注释提示框功能（王智博）

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,20 @@
   <div id="app">
     <router-view />
     <service-worker-update-popup />
-    <freight-config-popup />
+    <!-- <freight-config-popup /> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import ServiceWorkerUpdatePopup from '@/pwa/components/ServiceWorkerUpdatePopup.vue'
-import FreightConfigPopup from '@/views/freightConfigPopup/index.vue'
+// import FreightConfigPopup from '@/views/freightConfigPopup/index.vue'
 
 @Component({
   name: 'App',
   components: {
-    ServiceWorkerUpdatePopup,
-    FreightConfigPopup
+    ServiceWorkerUpdatePopup
+    // FreightConfigPopup
   }
 })
 export default class extends Vue {}
