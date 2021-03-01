e9819de4822e4b6a87c1795c4cd674a32609e541
wanglufei <wangzhibo@yunniao.me>
2021-03-01 17:21:44 +0800

    设置policymock（王智博）

--- src/api/clue.ts
+++ src/api/clue.ts
@@ -6,3 +6,22 @@ export const configurationManagementList = (data: any) =>
     method: 'post',
     data
   })
+// 获取小组  设置policy
+
+export const getUserGroup = (params: any) =>
+  request({
+    url: `${prefix}/v2/market-clue/config/get/user-group`,
+    params
+  })
+// 获取小组人员
+export const getGroupUser = (params: any) =>
+  request({
+    url: `${prefix}/v2/market-clue/user-group/user`,
+    params
+  })
+// 设置 policy
+export const setPolicy = (data:any) => request({
+  url: `${prefix}/v2/market-clue/config/update/policy`,
+  method: 'post',
+  data
+})
--- src/views/clue/components/SetUpPolicy.vue
+++ src/views/clue/components/SetUpPolicy.vue
@@ -4,68 +4,65 @@
     title="设置policy"
     v-bind="$attrs"
     :width="'40%'"
+    destroy-on-close
+    :confirm="onConfirm"
     v-on="$listeners"
+    @close="resetForm"
+    @open="getUserGroupList"
   >
     <el-form
       :model="queryInfo"
       label-width="80px"
       label-position="left"
     >
-      <h3>不可接受线索人员</h3>
-      <el-form-item label="小组">
-        <el-select
-          v-model="queryInfo.group"
-          placeholder="请选择活动区域"
-        >
-          <el-option
-            label="区域一"
-            value="shanghai"
-          />
-          <el-option
-            label="区域二"
-            value="beijing"
-          />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="人员">
-        <el-checkbox-group v-model="queryInfo.person">
-          <el-checkbox
-            label="美食/餐厅线上活动"
-            name="type"
-          />
-          <el-checkbox
-            label="地推活动"
-            name="type"
-          />
-          <el-checkbox
-            label="线下主题活动"
-            name="type"
-          />
-          <el-checkbox
-            label="单纯品牌曝光"
-            name="type"
-          />
-        </el-checkbox-group>
-      </el-form-item>
-      <p class="setup-title">
-        设置成功后，随机分配线索时，选中的人员将不能接收到线索
-      </p>
+      <template v-if="isAutoSetpolicy">
+        <h3>不可接受线索人员</h3>
+        <el-form-item label="小组">
+          <el-select
+            v-model="queryInfo.group"
+            placeholder="请选择活动区域"
+            @change="usreGroupChange"
+          >
+            <el-option
+              v-for="item in userGroupList"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+            />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="人员">
+          <el-checkbox-group
+            v-model="queryInfo.notReceiveId"
+          >
+            <el-checkbox
+              v-for="item in teamMemberList"
+              :key="item.id"
+              :label="item.id"
+              name="type"
+            >
+              {{ item.name }}
+            </el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+        <p class="setup-title">
+          设置成功后，随机分配线索时，选中的人员将不能接收到线索
+        </p>
+      </template>
       <h3>设置线索回流时间</h3>
       <el-form-item label="待跟进">
         <el-input-number
-          v-model="queryInfo.num[0]"
+          v-model="queryInfo.followDayNum"
           :min="1"
           :max="10"
-          label="jkj"
           class="num-input-setup-clue"
         />
       </el-form-item>
-      <el-form-item label="待跟进">
+      <el-form-item label="跟进中">
         <el-input-number
-          v-model="queryInfo.num[1]"
+          v-model="queryInfo.followingDayNum"
           :min="1"
           :max="10"
-          label="jkj"
           class="num-input-setup-clue"
         />
       </el-form-item>
@@ -76,8 +73,9 @@
   </SelfDialog>
 </template>
 <script lang="ts">
-import { Component, Vue, Prop } from 'vue-property-decorator'
+import { Component, Prop, Vue } from 'vue-property-decorator'
 import SelfDialog from '@/components/SelfDialog/index.vue'
+import { getUserGroup, getGroupUser, setPolicy } from '@/api/clue'
 @Component({
   name: 'SetUpPolicy',
   components: {
@@ -85,17 +83,85 @@ import SelfDialog from '@/components/SelfDialog/index.vue'
   }
 })
 export default class extends Vue {
+  @Prop({ default: () => {} }) policyData: any
   private queryInfo: any = {
     group: '',
-    person: [],
+    notReceiveId: [],
     toFollowed: [],
-    num: [0, 0]
+    followDayNum: undefined,
+    followingDayNum: undefined
   }
-  form: any = {}
-  isShowDialogc: boolean = true
-  cc(val: Function) {
-    this.isShowDialogc = false
-    console.log(val())
+  get isAutoSetpolicy() {
+    return this.policyData.distributionType === 0
+  }
+  private userGroupList: any = []
+  // 获取小组
+  async getUserGroupList() {
+    try {
+      const { data } = await getUserGroup({})
+      this.userGroupList = data.data
+      console.log(data)
+    } catch (error) {
+      return error
+    }
+  }
+  usreGroupChange(item:any) {
+    this.getTeamMember(item)
+  }
+  // 小组人员改变
+  private teamMemberList:any = []
+  async getTeamMember(groudID:string) {
+    try {
+      const { data } = await getUserGroup({
+        groudID
+      })
+      this.teamMemberList = data.data
+      console.log(data)
+    } catch (error) {
+      return error
+    }
+  }
+  resetForm() {
+    this.$nextTick(() => {
+      this.queryInfo = {
+        group: '',
+        notReceiveId: [],
+        toFollowed: [],
+        followDayNum: undefined,
+        followingDayNum: undefined
+      }
+    })
+  }
+  async setPolicyConfirm(callback:Function) {
+    try {
+      const { notReceiveId, id, followingDayNum, followDayNum } = this.queryInfo
+      let params = {
+        notReceiveId: notReceiveId.join(','),
+        id: this.policyData.id,
+        followingDayNum: followingDayNum || '',
+        followDayNum: followingDayNum || ''
+      }
+      const { data } = await setPolicy(params)
+      if (data.success) {
+        this.$message({
+          message: '操作成功',
+          type: 'success'
+        })
+        callback()
+        this.$emit('success')
+      } else {
+        this.$message({
+          message: data.errorMsg,
+          type: 'error'
+        })
+      }
+      console.log(data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  onConfirm(callback:Function) {
+    this.setPolicyConfirm(callback)
   }
 }
 </script>
@@ -103,11 +169,10 @@ export default class extends Vue {
 <style lang="scss" >
 .dasdadad {
   color: #333;
-  .el-dialog__body{
-  padding: 0px 30px 40px;
-
+  .el-dialog__body {
+    padding: 0px 30px 40px;
   }
-  h3{
+  h3 {
     margin: 40px 0;
   }
   .el-input-number {
@@ -153,7 +218,7 @@ export default class extends Vue {
       }
     }
   }
-  .setup-title{
+  .setup-title {
     color: #999;
     font-size: 16px;
     margin: 18px 0 22px 0;
