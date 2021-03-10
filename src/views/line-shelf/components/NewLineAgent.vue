9b7894a56d85e3775a6a8c1eb0beb68f2f8683bb
wanglufei <wangzhibo@yunniao.me>
2021-03-10 09:33:44 +0800

    详情字段构建（王智博）

--- src/views/line-shelf/components/NewLineAgent.vue
+++ src/views/line-shelf/components/NewLineAgent.vue
@@ -24,8 +24,55 @@
       </el-col>
     </el-row>
     <!-- 基础信息  -->
-    <section-container title="基础信息" />
-    <section-container title="仓策展信息" />
+    <section-container title="基础信息">
+      <self-form
+        :form-item="baseItem"
+        :list-query="baseInfo"
+      />
+    </section-container>
+    <section-container title="仓策展信息">
+      <self-form
+        :form-item="curationItem"
+        :list-query="baseInfo"
+      />
+      <el-row
+        :gutter="10"
+        class="curation-row"
+      >
+        <el-col
+          :span="6"
+          class="curation-col"
+        >
+          <div class="curation-title">
+            仓库图片
+          </div>
+        </el-col>
+        <el-col
+          :span="6"
+          class="curation-col"
+        >
+          <div class="curation-title">
+            货物图片
+          </div>
+        </el-col>
+        <el-col
+          :span="6"
+          class="curation-col"
+        >
+          <div class="curation-title">
+            装货图片
+          </div>
+        </el-col>
+        <el-col
+          :span="6"
+          class="curation-col"
+        >
+          <div class="curation-title">
+            讲解视频
+          </div>
+        </el-col>
+      </el-row>
+    </section-container>
     <section-container title="配送信息" />
     <section-container title="配送要求" />
     <section-container title="货物信息" />
@@ -68,13 +115,78 @@
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator'
 import SectionContainer from '@/components/SectionContainer/index.vue'
+import SelfForm from '@/components/Base/SelfForm.vue'
 @Component({
   name: 'NewLineAgent',
   components: {
-    SectionContainer
+    SectionContainer,
+    SelfForm
   }
 })
-export default class extends Vue {}
+export default class extends Vue {
+  private baseItem = [
+    {
+      type: 7,
+      label: '线路名称:',
+      key: 'lineName',
+      tagAttrs: {
+        class: 'active'
+      }
+    },
+    {
+      type: 7,
+      label: '线路编号:',
+      key: 'lineNo',
+      tagAttrs: {
+        class: 'active'
+      }
+    },
+    {
+      type: 7,
+      label: '窗口期:',
+      key: 'windowDate',
+      tagAttrs: {
+        class: 'error'
+      }
+    },
+    {
+      type: 7,
+      label: '线路分类:',
+      key: 'lineType'
+    },
+    {
+      type: 7,
+      label: '外线销售:',
+      key: 'lineType'
+    },
+    {
+      type: 7,
+      label: '客推经理:',
+      key: 'lineType'
+    }
+  ]
+  private curationItem = [
+    {
+      type: 7,
+      label: '仓名称:',
+      key: 'lineName',
+      tagAttrs: {
+        class: 'active'
+      }
+    },
+    {
+      type: 7,
+      label: '仓位置:',
+      key: 'lineNo',
+      tagAttrs: {
+        class: 'active'
+      }
+    }
+  ]
+  private baseInfo = {
+    checkbox: 'fadfas'
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -89,25 +201,40 @@ export default class extends Vue {}
   background: #fff;
 }
 .agent-button {
-  margin:20px 0;
+  margin: 20px 0;
   width: 100% !important;
   border: none;
+}
+.curation-row{
+  padding: 15px;
+}
+.curation-col{
+
 }
 </style>
 
 <style lang="scss" scoped>
 .new-line-container {
-  ::v-deep.SectionContainer {
+  ::v-deep .SectionContainer {
     margin-top: 8px;
     box-shadow: none;
     .title {
       border: none;
+    }
+    .context{
+      padding: 12px 30px;
     }
   }
   .el-button--info.is-plain:hover,
   .el-button--info.is-plain:focus {
     background: #ccc;
     color: #333;
+  }
+  ::v-deep .active {
+    color: #4899d0;
+  }
+  ::v-deep .error {
+    color: red;
   }
 }
 </style>
