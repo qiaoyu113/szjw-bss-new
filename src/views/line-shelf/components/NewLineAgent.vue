5f49a7686e3284573678eac70c28f3dc9c18e655
wanglufei <wangzhibo@yunniao.me>
2021-03-18 14:33:08 +0800

    页面构建

--- src/views/line-shelf/components/NewLineAgent.vue
+++ src/views/line-shelf/components/NewLineAgent.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="new-line-container">
     <el-row
-      :gutter="40"
       class="row-status"
     >
       <el-col
-        :span="3"
+        :span="8"
         class="col-status"
       >
-        代维护 100条
+        <span class="number">100</span>
+        <span class="title">待维护（条）</span>
       </el-col>
       <el-col
-        :span="3"
+        :span="8"
         class="col-status"
       >
-        已维护 100条
+        <span class="number">100</span>
+        <span class="title">已维护（条）</span>
       </el-col>
       <el-col
-        :span="3"
+        :span="8"
         class="col-status"
       >
-        今日完成 100条
+        <span class="number">100</span>
+        <span class="title">今日完成（条）</span>
       </el-col>
     </el-row>
     <!-- 基础信息  -->
@@ -47,7 +49,7 @@
             <span>仓库图片</span>
             <div>
               <img
-                src="https://www.baidu.com/img/PCpad_012830ebaa7e4379ce9a9ed1b71f7507.png"
+                src="https://t7.baidu.com/it/u=825057118,3516313570&fm=193&f=GIF"
                 alt=""
               >
             </div>
@@ -58,10 +60,10 @@
           class="curation-col"
         >
           <div class="curation-title">
-            <span>货物图片</span>
+            <span>货物图片(1)</span>
             <div>
               <img
-                src="https://www.baidu.com/img/PCpad_012830ebaa7e4379ce9a9ed1b71f7507.png"
+                src="https://t7.baidu.com/it/u=825057118,3516313570&fm=193&f=GIF"
                 alt=""
               >
             </div>
@@ -72,10 +74,10 @@
           class="curation-col"
         >
           <div class="curation-title">
-            <span>装货图片</span>
+            <span>装货图片(2)</span>
             <div>
               <img
-                src="https://www.baidu.com/img/PCpad_012830ebaa7e4379ce9a9ed1b71f7507.png"
+                src="https://t7.baidu.com/it/u=825057118,3516313570&fm=193&f=GIF"
                 alt=""
               >
             </div>
@@ -86,10 +88,10 @@
           class="curation-col"
         >
           <div class="curation-title">
-            <span>讲解视频</span>
+            <span>讲解视频(1)</span>
             <div>
               <img
-                src="https://www.baidu.com/img/PCpad_012830ebaa7e4379ce9a9ed1b71f7507.png"
+                src="https://t7.baidu.com/it/u=825057118,3516313570&fm=193&f=GIF"
                 alt=""
               >
             </div>
@@ -98,16 +100,147 @@
       </el-row>
     </section-container>
     <section-container title="标签信息">
-      <self-form
-        :list-query="listQuery"
-        :form-item="formItem"
-      />
+      <el-form
+        ref="form"
+        :model="form"
+        label-width="90px"
+      >
+        <el-row :gutter="10">
+          <el-col :span="7">
+            <el-form-item label="是否爆款">
+              <el-select
+                v-model="form.region1"
+                placeholder="请选择"
+              >
+                <el-option
+                  label="区域一"
+                  value="shanghai"
+                />
+                <el-option
+                  label="区域二"
+                  value="beijing"
+                />
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="7">
+            <el-form-item
+              label="线路肥瘦"
+              class="question-item"
+            >
+              <el-tooltip
+                placement="top"
+                effect="light"
+              >
+                <div slot="content">
+                  说明：<br>
+                  北京市<br>
+                  专车司机： 超肥、单肥、次肥、中肥、极瘦<br>
+                  共享司机：次肥、中瘦、极瘦
+                </div>
+                <i class="el-icon-question" />
+              </el-tooltip>
+              <el-select
+                v-model="form.region1"
+                placeholder="请选择活动区域"
+              >
+                <el-option
+                  label="区域一"
+                  value="shanghai"
+                />
+                <el-option
+                  label="区域二"
+                  value="beijing"
+                />
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="7">
+            <el-form-item
+              label="是否万金油"
+              class="question-item"
+            >
+              <el-tooltip
+                placement="top"
+                effect="light"
+                popper-class="adsadada"
+              >
+                <template #content>
+                  提示<br>
+                  如：4.2米干厢货晚上干活，4.2米白天活，金杯下午活
+                </template>
+                <i
+                  class="el-icon-question"
+                  style=""
+                />
+              </el-tooltip>
+              <el-select
+                v-model="form.region"
+                placeholder="请选择活动区域"
+              >
+                <el-option
+                  label="区域一"
+                  value="shanghai"
+                />
+                <el-option
+                  label="区域二"
+                  value="beijing"
+                />
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col
+            :span="16"
+            class="group-check"
+          >
+            <el-form-item
+              class="checkbox-all"
+              label="是否万金油"
+              label-width="100%"
+            >
+              <el-checkbox-group
+                v-model="form.ccc"
+                size="medium"
+              >
+                <el-checkbox-button
+                  v-for="city in cities"
+                  :key="city"
+                  class="check-label"
+                  :label="city"
+                >
+                  {{ city }}
+                </el-checkbox-button>
+              </el-checkbox-group>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
     </section-container>
 
-    <section-container title="配送信息" />
-    <section-container title="配送要求" />
-    <section-container title="货物信息" />
-    <section-container title="结算信息" />
+    <section-container title="配送信息">
+      <self-form
+        :form-item="deliveryItem"
+        :list-query="deliveryInfo"
+      />
+    </section-container>
+    <section-container title="配送要求">
+      <self-form
+        :form-item="deliveryDemandItme"
+        :list-query="deliveryDemandInfo"
+      />
+    </section-container>
+    <section-container title="货物信息">
+      <self-form
+        :form-item="deliveryDemandItme"
+        :list-query="deliveryDemandInfo"
+      />
+    </section-container>
+    <section-container title="结算信息">
+      <self-form
+        :form-item="deliveryDemandItme"
+        :list-query="deliveryDemandInfo"
+      />
+    </section-container>
 
     <!-- 按钮 -->
     <el-row :gutter="20">
@@ -160,6 +293,7 @@ export default class extends Vue {
       type: 7,
       label: '线路名称:',
       key: 'lineName',
+      col: 4,
       tagAttrs: {
         class: 'active'
       }
@@ -168,6 +302,7 @@ export default class extends Vue {
       type: 7,
       label: '线路编号:',
       key: 'lineNo',
+      col: 4,
       tagAttrs: {
         class: 'active'
       }
@@ -176,22 +311,28 @@ export default class extends Vue {
       type: 7,
       label: '窗口期:',
       key: 'windowDate',
+      col: 4,
       tagAttrs: {
         class: 'error'
       }
     },
     {
       type: 7,
+      col: 4,
       label: '线路分类:',
       key: 'lineType'
     },
     {
       type: 7,
+      col: 4,
+
       label: '外线销售:',
       key: 'lineType'
     },
     {
       type: 7,
+      col: 4,
+
       label: '客推经理:',
       key: 'lineType'
     }
@@ -201,6 +342,7 @@ export default class extends Vue {
       type: 7,
       label: '仓名称:',
       key: 'lineName',
+      col: 4,
       tagAttrs: {
         class: 'active'
       }
@@ -209,6 +351,7 @@ export default class extends Vue {
       type: 7,
       label: '仓位置:',
       key: 'lineNo',
+      col: 4,
       tagAttrs: {
         class: 'active'
       }
@@ -217,30 +360,163 @@ export default class extends Vue {
   private baseInfo = {
     checkbox: 'fadfas'
   }
-  private listQuery ={
+  private listQuery = {}
+  private hasOption = [
+    {
+      leble: '是',
+      name: '0'
+    },
+    {
+      leble: '否',
+      name: '1'
+    }
+  ]
+  private form = {
+    name: '',
+    region: '',
+    region1: '',
+    region2: '',
+    ccc: []
+  }
+  private cities = ['上海', '北京', '广州', '深圳']
+  private deliveryItem = [
+    {
+      type: 7,
+      label: '车型:',
+      key: 'lineName'
+    },
+    {
+      type: 7,
+      label: '配送区域:',
+      key: 'lineName'
+    },
+    {
+      type: 7,
+      label: '详细地址',
+      key: 'lineName'
+    },
+    {
+      type: 7,
+      label: '是否走禁行:',
+      key: 'lineName'
+    },
+    {
+      type: 7,
+      label: '是否走限行:',
+      key: 'lineName'
+    },
+    {
+      type: 7,
+      label: '油电要求:',
+      key: 'lineName'
+    },
+    {
+      type: 7,
+      label: '装卸要求:',
+      key: 'lineName'
+    },
+    {
+      type: 7,
+      label: '是否走高速:',
+      key: 'lineName'
+    },
+    {
+      type: 7,
+      label: '是否需要回单:',
+      key: 'lineName'
+    },
+    {
+      type: 7,
+      label: '服务要求和备注:',
+      key: 'lineName'
+    }
+  ]
+  private deliveryInfo = {}
+  private deliveryDemandItme = [
+    {
+      type: 7,
+      label: '配送时间:',
+      key: 'lineName'
+    },
+    {
+      type: 7,
+      label: '预计月出车天数:',
+      key: 'lineName'
+    }, {
+      type: 7,
+      label: '每日出车趟数:',
+      key: 'lineName'
+    }, {
+      type: 7,
+      label: '预计工作时间:',
+      key: 'lineName'
+    }, {
+      type: 7,
+      label: '配送数量:',
+      key: 'lineName'
+    }, {
+      type: 7,
+      label: '配送里程:',
+      key: 'lineName'
+    }
+  ]
+  private deliveryDemandInfo ={}
+  private goodsItem = [
+    {
+      type: 7,
+      label: '货物类型:',
+      key: 'lineName'
+    },
+    {
+      type: 7,
+      label: '货物件数:',
+      key: 'lineName'
+    }, {
+      type: 7,
+      label: '是否需要搬运:',
+      key: 'lineName'
+    }, {
+      type: 7,
+      label: '货物体积:',
+      key: 'lineName'
+    }, {
+      type: 7,
+      label: '货物重量:',
+      key: 'lineName'
+    }, {
+      type: 7,
+      label: '其他上岗要求:',
+      key: 'lineName'
+    }
+  ]
+  private goodsInfo = {
 
   }
-  private hasOption = [{
-    leble: '是',
-    name: '0'
-  },
-  {
-    leble: '否',
-    name: '1'
-  }
-  ]
-  private formItem =[
+  private SettlementItem = [
     {
-      type: 2,
-      tagAttrs: {
-        placeholder: '请选择',
-        clearable: true,
-        filterable: true
-      },
-      label: '业务线',
-      key: 'busiType',
-      col: 8,
-      options: this.hasOption
+      type: 7,
+      label: '单趟报价:',
+      key: 'lineName'
+    }, {
+      type: 7,
+      label: '单趟提成报价:',
+      key: 'lineName'
+    }, {
+      type: 7,
+      label: '预计月报价:',
+      key: 'lineName'
+    }, {
+      type: 7,
+      label: '计价方式:',
+      key: 'lineName'
+    }, {
+      type: 7,
+      label: '结算周期:',
+      key: 'lineName'
+    }, {
+      type: 7,
+      label: '结算天数:',
+      key: 'lineName'
     }
   ]
 }
@@ -248,43 +524,78 @@ export default class extends Vue {
 
 <style lang="scss" scoped>
 .row-status {
-  background: #f2f2f2;
   margin: 0 !important;
+  background: linear-gradient(225deg, #64C9EE 0%, #649CEE 100%);
+  padding: 18px 0;
 }
 .col-status {
-  border-radius: 3px;
-  margin: 14px 12px;
-  padding: 18px;
-  background: #fff;
+  border-right:1px solid #EFF5FE;
+  padding: 4px 0;
+  color: #FFF;
+  &:nth-last-child(1){
+    border: none;
+  }
+  span{
+    display:block;
+    text-align: center;
+    width: 100%;
+  }
+  .number{
+    font-size: 18px;
+    margin-bottom:4px ;
+  }
+  .title{
+    font-size: 14px;
+  }
 }
 .agent-button {
   margin: 20px 0;
   width: 100% !important;
   border: none;
 }
-.curation-row{
+.curation-row {
   padding: 15px;
 }
-.curation-col{
-
-.curation-title{
-  display: flex;
-  flex-wrap: wrap;
-  span{
-        width: 100%;
-    display: inline-block;
-  }
-  div{
-    width: 80%;
-    height: 200px;
-    border: 1px solid #eee;
-    margin-top:12px;
-  }
-  img{
-    width: 100%;
-    height: 100%;
+.curation-col {
+  .curation-title {
+    display: flex;
+    font-size: 0;
+    span {
+      font-size: 14px;
+      color: #838383;
+      line-height: 26px;
+      max-width: 120px;
+      margin-right:16px ;
+      display: inline-block;
+    }
+    div {
+      flex: 1;
+      width: 260px;
+      height: 160px;
+      border-radius: 5px;
+      overflow: hidden;
+    }
+    img {
+      width: 100%;
+      height: 100%;
+    }
   }
 }
+// 按钮组的样式
+.check-label {
+  margin-right: 20px;
+}
+::v-deep .el-checkbox-button__inner {
+  border: none;
+  border: 1px solid #dcdfe6 !important;
+  border-radius: 4px !important;
+  box-sizing: border-box;
+}
+::v-deep .is-checked {
+  .el-checkbox-button__inner {
+    border: 1px solid #649cee !important;
+    box-shadow: none !important;
+  }
 }
 </style>
 
@@ -293,11 +604,12 @@ export default class extends Vue {
   ::v-deep .SectionContainer {
     margin-top: 8px;
     box-shadow: none;
+    border-bottom: 2px solid #F8F9FA;
     .title {
       border: none;
     }
-    .context{
-      padding: 12px 30px;
+    .context {
+      padding: 0px 30px;
     }
   }
   .el-button--info.is-plain:hover,
@@ -310,6 +622,47 @@ export default class extends Vue {
   }
   ::v-deep .error {
     color: red;
+  }
+  .el-icon-question {
+    font-size: 18px;
+    line-height: 36px;
+    margin-right: 6px;
+  }
+  ::v-deep .el-form-item{
+    margin-bottom: 12px !important;
+  }
+  .question-item {
+    ::v-deep .el-form-item__label {
+      padding: 0 !important;
+      margin-right: 6px;
+    }
+    .el-select {
+      width: calc(100% - 50px);
+    }
+  }
+  .checkbox-all {
+    display: flex;
+    flex-wrap: wrap;
+    ::v-deep .el-form-item__label {
+      text-align: left;
+    }
+    ::v-deep .el-form-item__content {
+      margin: 0 !important;
+    }
+  }
+}
+.group-check {
+  margin: 20px 0 0 20px;
+}
+</style>
+<style lang="scss">
+.adsadada{
+  border: none !important;
+  box-shadow: 0 -2px 6px 0 rgba(0,0,0,0.24);
+  .popper__arrow{
+    border: none;
+  box-shadow: 0 -2px 6px 0 rgba(0,0,0,0.24);
+
   }
 }
 </style>
