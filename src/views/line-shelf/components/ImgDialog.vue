dd48d17f0a0819f3be3d15661568cdb3e0a424dd
wanglufei <wangzhibo@yunniao.me>
2021-03-20 18:04:00 +0800

    预览

--- src/views/line-shelf/components/ImgDialog.vue
+++ src/views/line-shelf/components/ImgDialog.vue
@@ -1,25 +1,40 @@
 <template>
-  <el-dialog
-    width="30%"
-    title="内层 Dialog"
-    :visible.sync="innerVisible"
-    append-to-body
-  />
+  <div>
+    <el-image-viewer
+      v-if="showImgViewer"
+      style="z-index:3000"
+      :on-close="closeViewer"
+      :url-list="imgPreviewList"
+    />
+    <button @click="showImgViewer=true">
+      dsafafdsfasdf
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator'
 import SelfDialog from '@/components/SelfDialog/index.vue'
+import ElImageViewer from 'element-ui/packages/image/src/image-viewer.vue'
 
 @Component({
   name: 'ImgDialog',
   components: {
-    SelfDialog
+    ElImageViewer
   }
 })
 export default class extends Vue {
   onConfirm(callback: Function) {
     // this.setPolicyConfirm(callback)
+  }
+  showImgViewer = true
+  imgPreviewList=[
+    'https://t7.baidu.com/it/u=825057118,3516313570&fm=193&f=GIF',
+    'https://5b0988e595225.cdn.sohucs.com/images/20180706/762c46951d624675ab88874a61a11eb5.jpeg',
+    'https://t7.baidu.com/it/u=825057118,3516313570&fm=193&f=GIF'
+  ]
+  closeViewer() {
+    this.showImgViewer = false
   }
 }
 </script>
