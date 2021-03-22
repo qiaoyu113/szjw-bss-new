d1e570717d83f0592c779df494129fc8c3e153ac
e <2370484962@qq.com>
2021-03-22 20:05:19 +0800

    返回路径（李威山）

--- src/views/preset/wtSystem/RoleManage.vue
+++ src/views/preset/wtSystem/RoleManage.vue
@@ -323,7 +323,7 @@ export default class extends Vue {
   }
   private goUserPage(id: string) {
     let name = this.sysTypeRoute()
-    this.$router.push({ path: `/roleSystem/${name}/showUser`, query: { id: id } })
+    this.$router.push({ path: `/roleSystem/${name}/showUser`, query: { id: id, redirect: this.$route.fullPath } })
   }
   private goCreateUser() {
     let name = this.sysTypeRoute()
