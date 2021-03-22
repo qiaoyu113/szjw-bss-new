80e35d390d33897b5134d06ad0dbd90472398e71
qianlei <qianlei@yunniao.me>
2021-03-22 17:09:58 +0800

    城市筛选添加权限（钱磊）

--- src/views/clue/policy/RandomlyAssigned.vue
+++ src/views/clue/policy/RandomlyAssigned.vue
@@ -153,6 +153,7 @@ export default class extends Vue {
         //   lazyLoad: this.cityList
         // }
       },
+      url: '',
       options: this.cityList,
       col: 8,
       label: '城市',
@@ -168,6 +169,12 @@ export default class extends Vue {
   ]
   // 获取获取城市列表
   async cityDetail() {
+    const aa = checkPermission(['/v2/market-clue/config/city'])
+    if (!aa) {
+      const { city: value, cityName: label } = this.tableData[0]
+      this.cityList.push({ value, label })
+      return
+    }
     let { data: city } = await GetDictionaryCity()
     if (city.success) {
       const nodes = city.data.map(function(item: any) {
@@ -281,18 +288,12 @@ export default class extends Vue {
   private async getList(this:any, order?:string) {
     try {
       this.listLoading = true
-      // const submitForm = this.clueArr.find((item: any) => {
-      //   return item.id === this.listQuery.clueType
-      // }) || {}
       let params:IState = {
         page: this.page.page,
         limit: this.page.limit,
         clueType: this.activeLineType,
         order: order || ''
       }
-      // if (this.listQuery.city && this.listQuery.city.length > 1) {
-      //   params.city = this.listQuery.city
-      // }
       this.listQuery.city && this.listQuery.city.length && (params.city = +this.listQuery.city[0])
       if (params.clueType === 0) {
         let { data: res } = await configurationWTSpecialList(params)
@@ -335,6 +336,9 @@ export default class extends Vue {
           this.$message.error(res.errorMsg)
         }
       }
+      if (!this.cityList[0]) {
+        this.cityDetail()
+      }
     } catch (err) {
       console.log(`get list fail:${err}`)
     } finally {
@@ -348,7 +352,6 @@ export default class extends Vue {
     this.$nextTick(() => {
       this.getList()
     })
-    this.cityDetail()
   }
 }
 </script>
