108223976321f1022e8c844d557c0e09c67ddc62
alin <934908531@qq.com>
2021-03-31 10:04:26 +0800

    线路货架维护已完成，冗余线路 详情 快照详情 （陈家琳）

--- src/api/line-shelf.ts
+++ src/api/line-shelf.ts
@@ -1,40 +1,12 @@
 import request from '@/utils/request'
 let prefix2 = '/mock/237'
 let prefix3 = '/mock/153'
-// 基础
-export const awitDetail = (params: any) =>
-  request({
-    url: `${prefix2}/awitDetail`,
-    method: 'get',
-    params
-  })
+let prefix4 = 'line_center'
 
-// 配送
-export const distribution = (params: any) =>
-  request({
-    url: `${prefix2}/awitDistribution`,
-    method: 'get',
-    params
-  })
-  // 结算
-export const settlemen = (params: any) =>
-  request({
-    url: `${prefix2}/settlement`,
-    method: 'get',
-    params
-  })
-  // 货物
-export const goods = (params: any) =>
-  request({
-    url: `${prefix2}/goods`,
-    method: 'get',
-    params
-  })
-
-// 已完成新线维护 Tab
+// 已完成 新线维护 Tab
 export const getFinishedLine = (data :any) => {
   return request({
-    url: `${prefix3}/v3/line/shelf/maintenance/queryLineShelfNewCompleted`,
+    url: `${prefix4}/v3/line/shelf/maintenance/queryLineShelfNewCompleted`,
     method: 'post',
     data
   })
@@ -50,11 +22,28 @@ export const getNewLineDetail = (data:any) => {
 // 已完成冗余线路维护  Tab
 export const getFinishedMoreLine = (data :any) => {
   return request({
-    url: `${prefix3}/v3/line/shelf/maintenance/queryLineListByConditionsQuery`,
+    url: `${prefix4}/v3/line/shelf/maintenance/queryLineListByConditionsQuery`,
     method: 'post',
     data
   })
 }
+// 获取线路详情信息
+export const getLineDetail = (params :any) => {
+  return request({
+    url: `${prefix4}/v2/line/lineInfo/detail`,
+    method: 'get',
+    params
+  })
+}
+// 获取快照详情信息
+export const getLineSnapshot = (params :any) => {
+  return request({
+    url: `${prefix4}/v3/line/shelf/maintenance/getLineShelfSnapshot`,
+    method: 'get',
+    params
+  })
+}
+
 // 获取an余线路列表
 export const getReaundanLineList = (data:any) =>
   request({
