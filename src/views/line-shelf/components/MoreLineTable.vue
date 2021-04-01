5ffb75dcaf03fbef31db569ab268fbe399f162f6
alin <934908531@qq.com>
2021-04-01 15:15:21 +0800

    线路货架维护 状态按钮小图标 字段更改（陈家琳）

--- src/views/line-shelf/components/MoreLineTable.vue
+++ src/views/line-shelf/components/MoreLineTable.vue
@@ -163,8 +163,8 @@ export default class extends Vue {
         this.listLoading = false
         this.tableData = res.data
         this.status[0].num = res.title.all >= 999 ? '999+' : res.title.all
-        this.status[1].num = res.title.removeShelvesNum >= 999 ? '999+' : res.title.all
-        this.status[2].num = res.title.ignoreNum >= 999 ? '999+' : res.title.all
+        this.status[1].num = res.title.removeShelvesNum >= 999 ? '999+' : res.title.removeShelvesNum
+        this.status[2].num = res.title.ignoreNum >= 999 ? '999+' : res.title.ignoreNum
       } else {
         this.$message.error('数据无法访问')
       }
