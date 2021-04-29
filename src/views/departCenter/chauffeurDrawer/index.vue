0279414afc3e745ab25f83ef255515746dce3c28
LiYC <liyungtsan@foxmail.com>
2021-04-29 16:56:33 +0800

    modified

--- src/views/departCenter/chauffeurDrawer/index.vue
+++ src/views/departCenter/chauffeurDrawer/index.vue
@@ -29,7 +29,7 @@
           :op-type="[]"
           @tag="setTagHandle"
           @call="setCallHandle"
-          @creatRun="creatRunHandle"
+          @creatRun="onCreateTryRun"
         />
       </section>
       <!-- 撮合匹配的司机列表 -->
@@ -52,6 +52,7 @@
       </section>
       <SetTag ref="tagShow" />
     </Scroll>
+    <CreateTryRun ref="tryRunShow" />
   </DrawerModel>
 </template>
 
@@ -85,6 +86,7 @@ export default class GuestDrawer extends Vue {
     private tagShow:boolean = false
     private pageSize:number = 1
     private tryRunShow:boolean = false
+    private showTryRun:boolean = false
     private rowData:object = {}
     private lineTableData:IState[] = [] // 线路列表
     private driverTableData:IState[] = [] // 司机列表
@@ -143,9 +145,9 @@ export default class GuestDrawer extends Vue {
     handleOpenClick() {
       AppModule.CloseSideBar(false)
     }
-    creatRunHandle(data:any) {
-      (this.$refs.tryRunShow as any).showDialog = true
-      this.rowData = data
+    onCreateTryRun(data:any) { // todo
+    //   (this.$refs.tryRunShow as any).showDialog = true
+    //   this.rowData = data
     }
     // 获取列表数据
     async getLists() {
@@ -185,9 +187,6 @@ export default class GuestDrawer extends Vue {
       } finally {
         console.log('')
       }
-    }
-    onCreateTryRun(driver: object) {
-      console.log('driver', driver)
     }
     loadMoreHandle() {
       this.getLists()
